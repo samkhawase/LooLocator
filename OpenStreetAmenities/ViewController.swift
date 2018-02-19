1408feca484bd58364a84662e5300f521b087e93
Sam Khawase <sam.khawase@storecast.de>
2018-02-19 09:02:12 +0100

    Initial Commit

---
+++ OpenStreetAmenities/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenStreetAmenities
+//
+//  Created by Sam Khawase on 14.02.18.
+//  Copyright © 2018 OpenStreetAmenities. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
